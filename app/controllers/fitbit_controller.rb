1a7c61561192ccd02d4c4d635be9edc3f01926b9
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-09 16:35:07 +0900

    add set_client #8

--- app/controllers/fitbit_controller.rb
+++ app/controllers/fitbit_controller.rb
@@ -1,4 +1,6 @@
 class FitbitController < ApplicationController
+  before_action :set_client
+
   def index
   end
 
@@ -28,4 +30,15 @@ class FitbitController < ApplicationController
 
   def weight
   end
+
+  private
+
+    def set_client
+      @client = FitbitAPI::Client.new(client_id: ENV['FITBIT_ID'],
+                               client_secret: ENV['FITBIT_SECRET'],
+                               access_token: ENV['FITBIT_ACCESS_TOKEN'],
+                               refresh_token: ENV['FITBIT_REFRESH_TOKEN'],
+                               expires_at: 1234567890,
+                               user_id: ENV['FITBIT_USER_ID'])
+    end
 end
